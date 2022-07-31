ad1b35f93a0b8c28d42312d7d50749d6e2d05793
stormsidali2001 <assoulsidali@gmail.com>
2022-07-31 15:39:30 +0200

    ...

--- frontend/components/feed/Feed.tsx
+++ frontend/components/feed/Feed.tsx
@@ -1,10 +1,12 @@
 import NewPost from '../new post/NewPost';
+import Post from '../post/Post';
 import styles from './Feed.module.scss';
 const Feed = ()=>{
     return(
         <div className={styles.container}>
             <div className={styles.wrapper}>
                 <NewPost/>
+                <Post username='sidali' imageUrl='/sidali.jpg' createdAt='1 min ago'/>
             </div>
         </div>
     )
